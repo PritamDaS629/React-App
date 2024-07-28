5f0244c660e9321b0bd28cf01f7ab04726ac8572
Pritam Das <1996pritamdas@gmail.com>
2024-07-28 21:17:26 +0530

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import  ReactDOM  from 'react-dom/client';
+//67862ab8
+
+
+import App from './App';
+
+
+const root =ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App/>);
+
